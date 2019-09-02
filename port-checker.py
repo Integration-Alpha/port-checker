533367f1acb72393d08662a714c687b4fd257fe1
Bal Balaji <bal.balaji@juliusbaer.com>
2019-09-02 15:51:23 +0200

    updated to use csv

--- port-checker.py
+++ port-checker.py
@@ -22,13 +22,28 @@ if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
 
-    parser.add_option("-a", "--address", dest="address", default='localhost', help="ADDRESS for server", metavar="ADDRESS")
+    #parser.add_option("-a", "--address", dest="address", default='localhost', help="ADDRESS for server", metavar="ADDRESS")
 
-    parser.add_option("-p", "--port", dest="port", type="int", default=80, help="PORT for server", metavar="PORT")
+    #parser.add_option("-p", "--port", dest="port", type="int", default=80, help="PORT for server", metavar="PORT")
+    parser.add_option("-f", "--filename", dest="filename", help="CSV file with list of addresses and ports", metavar="FILENAME")
 
     (options, args) = parser.parse_args()
-    print 'options: %s, args: %s' % (options, args)
-    check = check_server(options.address, options.port)
-    print 'check_server returned %s' % check
+    file = open(options.filename, "r")
+    lines = file.readlines()
+    for line in lines:
+
+        address = line.split(',')[0]
+        port = int(line.split(',')[1])
+
+        check = check_server(address, port)
+        print(line)
+        print 'check_server returned %s' % check
+
+    file.close()
+
+
+
+    
+
 
     sys.exit(not check)
